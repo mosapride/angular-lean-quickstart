93e4657aa563c3a8844ba8c0f19c60c9c852d7d5
mosapride <mosapride01@gmail.com>
2017-04-20 17:36:00 +0900

    3-master-detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,23 @@ export class Hero {
   id: number;
   name: string;
 }
+const HEROES: Hero[] = [
+  { id: 11, name: 'ベジータ' },
+  { id: 12, name: '緋村剣心' },
+  { id: 13, name: 'ガッツ' },
+  { id: 14, name: 'ケンシロウ' },
+  { id: 15, name: 'リヴァイ' },
+// 調子に乗りすぎて多すぎ。見にくい。
+//  { id: 16, name: 'エドワード・エルリック' },
+//  { id: 17, name: '空条承太郎' },
+//  { id: 18, name: '浦飯幽助' },
+//  { id: 19, name: 'ナツ・ドラグニル' },
+//  { id: 20, name: 'モンキー・D・ルフィ' },
+//  { id: 21, name: '黒崎一護' },
+//  { id: 22, name: 'アーカード' },
+//  { id: 23, name: 'うずまきナルト' },
+//  { id: 24, name: '孫悟空' }
+];
 
 @Component({
   selector: 'app-root',
@@ -12,9 +29,12 @@ export class Hero {
 })
 export class AppComponent {
   title = 'ヒーローズツアー';
-  hero: Hero = {
-    id: 1,
-    name: '暴風マン'
-  };
+  heros = HEROES;
+  hoge1 = 'dummy1';
+  hoge2 = 'dummy2';
 
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
